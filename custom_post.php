785dca8d6d71889d4b8680e5f7de18932a09486e
ecastaneda <ecastaneda@prensalibre.com.gt>
2016-09-30 17:20:42 -0600

    custom fields, metaboxes y custom post
    modifico el custom post para que acepte taxonomías

--- custom_post.php
+++ custom_post.php
@@ -2,49 +2,78 @@
 <?php
 function horarios_post_type() {
 
-// Etiquetas para horarios
-	$labels = array(
-		'name'                => _x( 'Horarios', 'guatevision' ),
-		'singular_name'       => _x( 'Horario', 'guatevision' ),
-		'menu_name'           => __( 'Horarios', 'guatevision' ),
-		'parent_item_colon'   => __( 'Horario Padre', 'guatevision' ),
-		'all_items'           => __( 'Todos los Horarios', 'guatevision' ),
-		'view_item'           => __( 'Ver Horario', 'guatevision' ),
-		'add_new_item'        => __( 'Agregar Nuevo Horario', 'guatevision' ),
-		'add_new'             => __( 'Agregar Nuevo', 'guatevision' ),
-		'edit_item'           => __( 'Editar Horario', 'guatevision' ),
-		'update_item'         => __( 'Actualizar Horario', 'guatevision' ),
-		'search_items'        => __( 'Buscar Horario', 'guatevision' ),
-		'not_found'           => __( 'Sin Resultados', 'guatevision' ),
-		'not_found_in_trash'  => __( 'Sin Resultadosen la Papelera', 'guatevision' ),
-	);
 
-// Opciones del tipo de post
+	// HORARIOS POST
+	function horarios_post_type() {
 
-	$args = array(
-		'label'               => __( 'horarios', 'guatevision' ),
-		'description'         => __( 'Horarios para programación', 'guatevision' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
-		'can_export'          => true,
-		'has_archive'         => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'capability_type'     => 'page',
+	// Etiquetas para horarios
+		$labels = array(
+			'name'                => _x( 'Horarios' ),
+			'singular_name'       => _x( 'Horario' ),
+			'menu_name'           => __( 'Horarios' ),
+			'parent_item_colon'   => __( 'Horario Padre' ),
+			'all_items'           => __( 'Todos los Horarios' ),
+			'view_item'           => __( 'Ver Horario' ),
+			'add_new_item'        => __( 'Agregar Nuevo Horario' ),
+			'add_new'             => __( 'Agregar Nuevo' ),
+			'edit_item'           => __( 'Editar Horario' ),
+			'update_item'         => __( 'Actualizar Horario' ),
+			'search_items'        => __( 'Buscar Horario' ),
+			'not_found'           => __( 'Sin Resultados' ),
+			'not_found_in_trash'  => __( 'Sin Resultadosen la Papelera' )
+		);
 
-		// This is where we add taxonomies to our CPT
-		'taxonomies'          => array( 'category' ),
-	);
+	// Opciones del tipo de post
 
-	register_post_type( 'horarios', $args );
+		$args = array(
+			'label'               => __( 'horarios' ),
+			'description'         => __( 'Horarios para programación' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields'),
+			'hierarchical'        => false,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_nav_menus'   => true,
+			'show_in_admin_bar'   => true,
+			'menu_position'       => 5,
+			'can_export'          => true,
+			'has_archive'         => true,
+			'exclude_from_search' => false,
+			'publicly_queryable'  => true,
+			'capability_type'     => 'page',
 
-}
+			// This is where we add taxonomies to our CPT
+			//'taxonomies'          => array( 'category' ),
+	                //'taxonomies'		  => array('topics', 'category' ),
+		);
 
-add_action( 'init', 'horarios_post_type', 0 );
+		register_post_type( 'horarios', $args );
+
+	}
+
+	add_action( 'init', 'horarios_post_type', 0 );
+
+	// DIAS PARA HORARIOS
+
+	function horarios_taxonomies() {
+	  $labels = array(
+	    'name'              => _x( 'Días de Horarios'),
+	    'singular_name'     => _x( 'Día de Horarios' ),
+	    'search_items'      => __( 'Buscar Día' ),
+	    'all_items'         => __( 'Todos' ),
+	    'parent_item'       => __( 'Día Padre' ),
+	    'parent_item_colon' => __( 'Día Padre:' ),
+	    'edit_item'         => __( 'Editar Día' ),
+	    'update_item'       => __( 'Actualizar Día' ),
+	    'add_new_item'      => __( 'Agregar Nuevo Día' ),
+	    'new_item_name'     => __( 'Nuevo Día' ),
+	    'menu_name'         => __( 'Días de Horarios' ),
+	  );
+	  $args = array(
+	    'labels' => $labels,
+	    'hierarchical' => true,
+	  );
+	  register_taxonomy( 'dia', 'horarios', $args );
+	}
+	add_action( 'init', 'horarios_taxonomies', 0 );
